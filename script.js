78e8b1b53003f29d24cf4cbfaabfe9dc18478733
Alexander Matte <alex.matte71@gmail.com>
2024-02-29 22:07:47 +0100

    created weather retrieving logic

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const key = "1026bbcaad5b47a48dc153630242202";
-const baseUrl = "http://api.weatherapi.com/v1";
+const baseUrl = "https://api.weatherapi.com/v1";
+let searchForm = document.querySelector("#search-form");
+let weatherLocation = document.querySelector(".location");
+
 
 
 
@@ -8,13 +11,41 @@ async function getWeather(location = "regina") {
   try {
     const response = await fetch(`${baseUrl}/current.json?key=${key}&q=${location}`, {mode: 'cors'});
     let data = await response.json();
-  
-    console.log(data);
-    
+    if(data.error) {
+        console.log(data.error.message);
+      return;
+    } else {
+      setCurrentWeather(data);
+      return data;
+    }
+
   } catch (error) {
     console.log(error);
   }
 
 }
 
-getWeather();
+
+
+searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  let searchValue = document.querySelector("#search-input").value;
+  getWeather(searchValue);
+ 
+})
+
+
+function setCurrentWeather (data) {
+  console.log(data);
+  let city = data.location.name;
+  let region = data.location.region;
+  let country = data.location.country;
+  let lastUpdated = data.current.last_updated;
+  document.querySelector(".description").innerHTML = data.current.condition.text;
+  document.querySelector(".temp").innerHTML = data.current.temp_c;
+
+
+  weatherLocation.innerHTML = `${city}, ${region}, ${country} As of ${lastUpdated}`
+
+}
+
