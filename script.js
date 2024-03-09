e29150d93d48b6764744b89513d4b9ccafedaa13
Alexander Matte <alex.matte71@gmail.com>
2024-03-09 22:25:28 +0100

    created basic template for weather forcast

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const key = "1026bbcaad5b47a48dc153630242202";
 const baseUrl = "https://api.weatherapi.com/v1";
 let searchForm = document.querySelector("#search-form");
-let weatherLocation = document.querySelector(".location");
+let weatherDiv = document.querySelector("#weather-info");
+
 
 
 
@@ -9,13 +10,13 @@ let weatherLocation = document.querySelector(".location");
 async function getWeather(location = "regina") {
 
   try {
-    const response = await fetch(`${baseUrl}/current.json?key=${key}&q=${location}`, {mode: 'cors'});
+    const response = await fetch(`${baseUrl}/forecast.json?days=3&key=${key}&q=${location}`, {mode: 'cors'});
     let data = await response.json();
     if(data.error) {
         console.log(data.error.message);
       return;
     } else {
-      setCurrentWeather(data);
+      setWeather(data);
       return data;
     }
 
@@ -27,25 +28,59 @@ async function getWeather(location = "regina") {
 
 
 
+
+
 searchForm.addEventListener("submit", e => {
   e.preventDefault();
+  weatherDiv.replaceChildren();
+  document.querySelector("#location").replaceChildren();
   let searchValue = document.querySelector("#search-input").value;
   getWeather(searchValue);
+  searchForm.reset();
  
 })
 
 
-function setCurrentWeather (data) {
-  console.log(data);
-  let city = data.location.name;
-  let region = data.location.region;
-  let country = data.location.country;
-  let lastUpdated = data.current.last_updated;
-  document.querySelector(".description").innerHTML = data.current.condition.text;
-  document.querySelector(".temp").innerHTML = data.current.temp_c;
+
+function setWeather (data) {
+  let current = data.current;
+  let location = data.location;
+  let forcastDays = data.forecast.forecastday;
+  let city = location.name;
+  let region = location.region;
+  let country = location.country;
+  document.querySelector("#location").innerHTML = `<p>${city}, ${region}, ${country}</p>`;
+  
+
+  forcastDays.forEach(item => {
+    let date = item.date;
+    let temp = item.day.avgtemp_c;
+    let icon = item.day.condition.icon;
+    let description = item.day.condition.text;
 
 
-  weatherLocation.innerHTML = `${city}, ${region}, ${country} As of ${lastUpdated}`
+    let weatherInfo = `
+    <div class="d-flex justify-content-between align-items-center border rounded">
+      <div class="d-flex flex-column">
+        <p>${date}</p>
+        <p>Temperature: ${temp}°C</p>
+        <p>${description}</p>
+      </div>
+      <div>
+        <img src="${icon}" alt="Weather Icon">
+      </div>
+    </div>
+
+
+    `;
+
+    let content = document.createElement("div").innerHTML = weatherInfo;
+    weatherDiv.innerHTML += content;
+
+
+  });
+
+
 
 }
 
